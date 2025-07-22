dfafd24f9bd2925825f37fc10fc9c0eb57f16402
olhanotolga <olha.halat@gmail.com>
2025-07-22 16:13:03 +0200

    feat: color cells

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { range } from '../../utils';
 import { NUM_OF_CHARS } from '../../constants';
+import { checkGuess } from '../../game-helpers';
+import { answer } from '../Game';
 
 function Guess({ guess }) {
   const cells = range(NUM_OF_CHARS);
 
+  const validatedCharacters = checkGuess(guess, answer);
+
   return (
     <p className='guess'>
       {cells.map((letterCell) => (
-        <span className='cell' key={letterCell}>
+        <span
+          className={`cell ${
+            validatedCharacters ? validatedCharacters[letterCell].status : ''
+          }`}
+          key={letterCell}
+        >
           {guess ? guess[letterCell] : ''}
         </span>
       ))}
